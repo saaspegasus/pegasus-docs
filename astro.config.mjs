eea2a7f86c5cd8243cba5a0a7fbe9ad74e0786e4
Simon Kelly <skelly@dimagi.com>
2025-07-21 12:05:31 +0200

    add llm.txt extension

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,135 +1,163 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import {defineConfig} from 'astro/config';
 import starlight from '@astrojs/starlight';
 import starlightLinksValidator from 'starlight-links-validator'
+import starlightLlmsTxt from 'starlight-llms-txt'
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://docs.saaspegasus.com/',
-	redirects: {
-		'/api-keys': '/apis',
-		'/using-virtualenvs': '/python/setup/',
-		'/python/': '/python/setup/',
-		'/front-end/': '/front-end/overview/',
-		'/css/': '/css/overview/',
-		'/ai/': '/ai/development/',
-		'/deployment/': '/deployment/overview/',
-		'/experimental/': '/experimental/react-front-end/',
-		'/community/': '/community/digital-ocean-spaces/',
-		'/deployment/google-cloud-github-actions/': '/community/google-cloud-github-actions/',
-	},
-	prefetch: true,
-	integrations: [
-		starlight({
-			plugins: [starlightLinksValidator({
-				errorOnLocalLinks: false,
-			})],
-			components: {
-				Footer: './src/components/Footer.astro',
-			},
-			title: 'Pegasus',
-			description: 'Documentation for SaaS Pegasus - the Django SaaS boilerplate',
-			favicon: '/favicon.svg',
-			social: [
-				{ 
-					icon: 'github', 
-					label: 'GitHub', 
-					href: 'https://github.com/saaspegasus/pegasus-docs' 
-				}
-			],
-			customCss: [
+  site: 'https://docs.saaspegasus.com/',
+  redirects: {
+    '/api-keys': '/apis',
+    '/using-virtualenvs': '/python/setup/',
+    '/python/': '/python/setup/',
+    '/front-end/': '/front-end/overview/',
+    '/css/': '/css/overview/',
+    '/ai/': '/ai/development/',
+    '/deployment/': '/deployment/overview/',
+    '/experimental/': '/experimental/react-front-end/',
+    '/community/': '/community/digital-ocean-spaces/',
+    '/deployment/google-cloud-github-actions/': '/community/google-cloud-github-actions/',
+  },
+  prefetch: true,
+  integrations: [
+    starlight({
+      plugins: [
+        starlightLinksValidator({
+          errorOnLocalLinks: false,
+        }),
+        starlightLlmsTxt({
+          details: "SaaS Pegasus is a Django-backed SaaS boilerplate that puts you in control. " +
+            "You choose the technologies and features you want included, and Pegasus generates a unique codebase for your project. " +
+            "Instead of starting with Django's default skeleton project, you start with a bunch of extra work done for you—and nothing you don't need.",
+          promote: ['index*', 'getting-started*', 'python/uv*', 'code-structure*', 'configuration*', '!*/*'],
+          exclude: ['release-nodes*', 'marketplace/*', 'community/*'],
+          customSets: [
+            {
+              label: 'Front End',
+              description: 'Pegasus\'s front-end architecture is a hybrid model, with a front-end codebase\n' +
+                'that is compiled and used directly in Django templates via Django\'s static files infrastructure.',
+              paths: ['front-end/**'],
+            },
+            {
+              label: 'CSS',
+              description: 'Documentation on Pegasus CSS setup and options.',
+              paths: ['css/**'],
+            },
+            {
+              label: 'Deployment',
+              description: 'Documentation for deploying Pegasus projects to different hosting providers.',
+              paths: ['css/**'],
+            },
+          ],
+        }),
+      ],
+      components: {
+        Footer: './src/components/Footer.astro',
+      },
+      title: 'Pegasus',
+      description: 'Documentation for SaaS Pegasus - the Django SaaS boilerplate',
+      favicon: '/favicon.svg',
+      social: [
+        {
+          icon: 'github',
+          label: 'GitHub',
+          href: 'https://github.com/saaspegasus/pegasus-docs'
+        }
+      ],
+      customCss: [
         './src/styles/custom.css',
       ],
-			head: [
-				// Google Analytics
-				{
-					tag: 'script',
-					attrs: {
-						async: true,
-						src: 'https://www.googletagmanager.com/gtag/js?id=UA-94102737-10',
-					},
-				},
-				{
-					tag: 'script',
-					content: `
+      head: [
+        // Google Analytics
+        {
+          tag: 'script',
+          attrs: {
+            async: true,
+            src: 'https://www.googletagmanager.com/gtag/js?id=UA-94102737-10',
+          },
+        },
+        {
+          tag: 'script',
+          content: `
 						window.dataLayer = window.dataLayer || [];
 						function gtag(){dataLayer.push(arguments);}
 						gtag('js', new Date());
 						gtag('config', 'UA-94102737-10');
 					`,
-				},
-				// Scriv chat integration
-				{
-					tag: 'script',
-					attrs: {
-						type: 'module',
-						src: 'https://unpkg.com/scriv-chat@0.2.0/dist/scriv-chat/scriv-chat.esm.js',
-					},
-				},
-			],
-			sidebar: [
-				{ label: 'Getting Started', slug: 'getting-started' },
-				{ label: 'GitHub Integration', slug: 'github' },
-				{ label: 'Customizations', slug: 'customizations' },
-				{ label: 'Configuration', slug: 'configuration' },
-				{ label: 'Code Structure', slug: 'code-structure' },
-				{
-					label: 'Python',
-					autogenerate: { directory: 'python' },
-					collapsed: true,
-				},
-				{
-					label: 'Front End',
-					autogenerate: { directory: 'front-end' },
-					collapsed: true,
-				},
-				{
-					label: 'CSS',
-					autogenerate: { directory: 'css' },
-					collapsed: true,
-				},
-				{ label: 'Page Metadata', slug: 'page-metadata' },
-				{ label: 'Docker', slug: 'docker' },
-				{ label: 'Teams', slug: 'teams' },
-				{ label: 'Subscriptions', slug: 'subscriptions' },
-				{ label: 'Payments', slug: 'payments' },
-				{ label: 'Forms', slug: 'forms' },
-				{ label: 'APIs', slug: 'apis' },
-				{
-					label: 'AI',
-					autogenerate: { directory: 'ai' },
-					collapsed: true,
-				},
-				{ label: 'Internationalization', slug: 'internationalization' },
-				{ label: 'Wagtail', slug: 'wagtail' },
-				{ label: 'Feature Flags', slug: 'flags' },
-				{ label: 'Celery', slug: 'celery' },
-				{ label: 'Async Support', slug: 'async' },
-				{
-					label: 'Deployment',
-					autogenerate: { directory: 'deployment' },
-					collapsed: true,
-				},
-				{ label: 'GitHub Actions', slug: 'github-actions' },
-				{ label: 'Cookbooks', slug: 'cookbooks' },
-				{
-					label: 'Experimental',
-					autogenerate: { directory: 'experimental' },
-					collapsed: true,
-				},
-				{ label: 'Upgrading', slug: 'upgrading' },
-				{
-					label: 'Community',
-					autogenerate: { directory: 'community' },
-					collapsed: true,
-				},
-				{
-					label: 'Marketplace',
-					autogenerate: { directory: 'marketplace' },
-					collapsed: true,
-				},
-				{ label: 'Release Notes', slug: 'release-notes' },
-			],
-		}),
-	],
+        },
+        // Scriv chat integration
+        {
+          tag: 'script',
+          attrs: {
+            type: 'module',
+            src: 'https://unpkg.com/scriv-chat@0.2.0/dist/scriv-chat/scriv-chat.esm.js',
+          },
+        },
+      ],
+      sidebar: [
+        {label: 'Getting Started', slug: 'getting-started'},
+        {label: 'GitHub Integration', slug: 'github'},
+        {label: 'Customizations', slug: 'customizations'},
+        {label: 'Configuration', slug: 'configuration'},
+        {label: 'Code Structure', slug: 'code-structure'},
+        {
+          label: 'Python',
+          autogenerate: {directory: 'python'},
+          collapsed: true,
+        },
+        {
+          label: 'Front End',
+          autogenerate: {directory: 'front-end'},
+          collapsed: true,
+        },
+        {
+          label: 'CSS',
+          autogenerate: {directory: 'css'},
+          collapsed: true,
+        },
+        {label: 'Page Metadata', slug: 'page-metadata'},
+        {label: 'Docker', slug: 'docker'},
+        {label: 'Teams', slug: 'teams'},
+        {label: 'Subscriptions', slug: 'subscriptions'},
+        {label: 'Payments', slug: 'payments'},
+        {label: 'Forms', slug: 'forms'},
+        {label: 'APIs', slug: 'apis'},
+        {
+          label: 'AI',
+          autogenerate: {directory: 'ai'},
+          collapsed: true,
+        },
+        {label: 'Internationalization', slug: 'internationalization'},
+        {label: 'Wagtail', slug: 'wagtail'},
+        {label: 'Feature Flags', slug: 'flags'},
+        {label: 'Celery', slug: 'celery'},
+        {label: 'Async Support', slug: 'async'},
+        {
+          label: 'Deployment',
+          autogenerate: {directory: 'deployment'},
+          collapsed: true,
+        },
+        {label: 'GitHub Actions', slug: 'github-actions'},
+        {label: 'Cookbooks', slug: 'cookbooks'},
+        {
+          label: 'Experimental',
+          autogenerate: {directory: 'experimental'},
+          collapsed: true,
+        },
+        {label: 'Upgrading', slug: 'upgrading'},
+        {
+          label: 'Community',
+          autogenerate: {directory: 'community'},
+          collapsed: true,
+        },
+        {
+          label: 'Marketplace',
+          autogenerate: {directory: 'marketplace'},
+          collapsed: true,
+        },
+        {label: 'Release Notes', slug: 'release-notes'},
+      ],
+    }),
+  ],
 });
